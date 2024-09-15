6b2129ce18855858474fcd7ff35ee600ce7accb8
JoaoGuiMB <juaogui159@gmail.com>
2024-09-15 11:07:22 -0300

    feat: create client use case

---
+++ internal/usecase/create_client/create_client_test.go
@@ -0,0 +1,39 @@
+package create_client
+
+import (
+	"testing"
+
+	"github.com.br/joaoguimb/fc-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	clientGatewayMock := &ClientGatewayMock{}
+	clientGatewayMock.On("Save", mock.Anything).Return(nil)
+	uc := NewCreateClientUseCase(clientGatewayMock)
+
+	input := &CreateClientInputDTO{
+		Name:  "John Doe",
+		Email: "john@doe.com",
+	}
+	output, err := uc.Execute(input)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, "John Doe", output.Name)
+	assert.Equal(t, "john@doe.com", output.Email)
+}
