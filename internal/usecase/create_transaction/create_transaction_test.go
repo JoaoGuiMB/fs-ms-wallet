01010aa42648bf4740ab03d04a7b9ddf28198e80
JoaoGuiMB <juaogui159@gmail.com>
2024-09-16 20:42:06 -0300

    feat: transaciton use case

---
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -0,0 +1,66 @@
+package createtransaction
+
+import (
+	"testing"
+
+	"github.com.br/joaoguimb/fc-ms-wallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateTransactionUseCase_Execute(t *testing.T) {
+	client1, _ := entity.NewClient("John Doe", "john@doe.com")
+	account1 := entity.NewAccount(client1)
+	account1.Credit(1000)
+
+	client2, _ := entity.NewClient("Jane Doe", "jane@doe.com")
+	account2 := entity.NewAccount(client2)
+	account2.Credit(1000)
+
+	accountGatewayMock := &AccountGatewayMock{}
+	accountGatewayMock.On("FindByID", account1.ID).Return(account1, nil)
+	accountGatewayMock.On("FindByID", account2.ID).Return(account2, nil)
+
+	transactionGatewayMock := &TransactionGatewayMock{}
+	transactionGatewayMock.On("Create", mock.Anything).Return(nil)
+
+	uc := NewCreateTransactionUseCase(transactionGatewayMock, accountGatewayMock)
+
+	input := &CreateTransactionInputDTO{
+		AccountIDFrom: account1.ID,
+		AccountIDTo:   account2.ID,
+		Amount:        100,
+	}
+
+	output, err := uc.Execute(input)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	accountGatewayMock.AssertExpectations(t)
+	transactionGatewayMock.AssertExpectations(t)
+	accountGatewayMock.AssertNumberOfCalls(t, "FindByID", 2)
+	transactionGatewayMock.AssertNumberOfCalls(t, "Create", 1)
+
+}
