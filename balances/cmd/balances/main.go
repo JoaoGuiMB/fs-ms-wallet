397b0478789cb296dae93dd7ffbf2ee530aefa7b
JoaoGuiMB <juaogui159@gmail.com>
2024-09-26 20:24:10 -0300

    feat: consuming and start web server

--- balances/cmd/balances/main.go
+++ balances/cmd/balances/main.go
@@ -5,6 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com.br/joaoguimb/fc-ms-wallet/balances/internal/database"
+	getbalance "github.com.br/joaoguimb/fc-ms-wallet/balances/internal/usecase/get_balance"
+	updatebalance "github.com.br/joaoguimb/fc-ms-wallet/balances/internal/usecase/update_balance"
+	"github.com.br/joaoguimb/fc-ms-wallet/balances/internal/web"
+	"github.com.br/joaoguimb/fc-ms-wallet/balances/internal/web/webserver"
 	"github.com.br/joaoguimb/fc-ms-wallet/balances/pkg/kafka"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	_ "github.com/go-sql-driver/mysql"
@@ -13,8 +18,8 @@ import (
 type MessagePayload struct {
 	AccountIDFrom      string `json:"account_id_from"`
 	AccountIDTo        string `json:"account_id_to"`
-	BalanceAccountFrom int64  `json:"balance_account_from"`
-	BalanceAccountTo   int64  `json:"balance_account_to"`
+	BalanceAccountFrom int    `json:"balance_account_from"`
+	BalanceAccountTo   int    `json:"balance_account_to"`
 }
 
 type TestMessage struct {
@@ -22,12 +27,15 @@ type TestMessage struct {
 	Payload MessagePayload
 }
 
-func main() {
-	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/balances?parseTime=true")
+func consumer() {
+	fmt.Println("running consumer")
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/balances?parseTime=true")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+
+	balancesDb := database.NewBalanceDB(db)
 
 	configMap := ckafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
@@ -50,4 +58,50 @@ func main() {
 	}
 	fmt.Println("aqui")
 	fmt.Println(testMessage.Payload)
+
+	updateBalanceUseCase := updatebalance.NewUpdateBalanceUseCase(balancesDb)
+
+	input := &updatebalance.UpdateBalanceInputDTO{
+		AccountIDFrom:        testMessage.Payload.AccountIDFrom,
+		AccountIDTo:          testMessage.Payload.AccountIDTo,
+		BalanceAccountIDFrom: testMessage.Payload.BalanceAccountFrom,
+		BalanceAccountIDTo:   testMessage.Payload.BalanceAccountTo,
+	}
+	output, err := updateBalanceUseCase.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(output)
+
+}
+
+func main() {
+	fmt.Println("Starting application")
+
+	// Set up the database connection
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3307)/balances?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	balancesDb := database.NewBalanceDB(db)
+
+	// Set up the web server
+	getBalanceUseCase := getbalance.NewGetBalanceUseCase(balancesDb)
+	webBalanceHandler := web.NewWebBalanceHanlder(*getBalanceUseCase)
+	webserver := webserver.NewWebServer(":3003")
+
+	// Start the web server in a goroutine
+	go func() {
+		webserver.AddHandler("/balances/", webBalanceHandler.GetBalance)
+		webserver.Start()
+		fmt.Println("Server started on port 3003")
+	}()
+
+	// Start the Kafka consumer
+	consumer()
+
+	// Prevent the main goroutine from exiting
+	select {}
 }
